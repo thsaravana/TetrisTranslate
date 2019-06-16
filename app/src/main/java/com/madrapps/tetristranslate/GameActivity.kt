36bbd8c0e738d13f25c59fc2c461c17d1a7c59b7
Henry <th.saravana@gmail.com>
2019-06-16 11:18:18 +0530

    Making it Full Screen

--- app/src/main/java/com/madrapps/tetristranslate/GameActivity.kt
+++ app/src/main/java/com/madrapps/tetristranslate/GameActivity.kt
@@ -3,10 +3,10 @@ package com.madrapps.tetristranslate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
     }
 }
