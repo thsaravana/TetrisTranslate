c3dbe206550f2970ec0200f6a24f078611883f2d
Henry <th.saravana@gmail.com>
2019-06-16 11:45:30 +0530

    Moving everything into a Fragment

--- app/src/main/java/com/madrapps/tetristranslate/GameActivity.kt
+++ app/src/main/java/com/madrapps/tetristranslate/GameActivity.kt
@@ -1,7 +1,7 @@
 package com.madrapps.tetristranslate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class GameActivity : AppCompatActivity() {
 
